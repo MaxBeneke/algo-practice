3c19bc83cd28d00c8454c34d9f24974562763dfd
Max Beneke <max.beneke@gmail.com>
2021-06-07 13:21:37 -0400

    SLL pop shift unshift

--- colt-steele/Data Structures/singlyLinkedListsIntro.js
+++ colt-steele/Data Structures/singlyLinkedListsIntro.js
@@ -22,4 +22,39 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    pop(){
+        if (!this.head) return undefined
+
+        let current = this.head
+        let newTail = current
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.length--;
+        this.tail.next = null;
+        return current;
+    }
+    shift() {
+        if (!this.head) return undefined
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } 
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
 }
