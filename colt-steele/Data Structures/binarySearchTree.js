8752610391441bf1b19f50fa0541d93daafc3a58
Max Beneke <max.beneke@gmail.com>
2021-06-14 10:55:44 -0400

    DFS In order

--- colt-steele/Data Structures/binarySearchTree.js
+++ colt-steele/Data Structures/binarySearchTree.js
@@ -85,4 +85,14 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+    DFSInOrder() {
+        var data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node.val);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
