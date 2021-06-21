39a688e18db579d6bd0cbac08d69a39f2aa43c04
Max Beneke <max.beneke@gmail.com>
2021-06-21 11:00:00 -0400

    basic hash function

--- colt-steele/Data Structures/hashTable.js
+++ colt-steele/Data Structures/hashTable.js
@@ -1,2 +1,13 @@
 let total = 0;
 total += "hello".charCodeAt(0) - 96
+
+//String Hash function
+
+function has(key, arrayLen) {
+    let total = 0;
+    for (let char of key) {
+        let value = char.charCodeAt(0) - 96;
+        total = (total + value) % arrayLen;
+    }
+    return total;
+}
