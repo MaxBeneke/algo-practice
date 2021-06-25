32a8c0a24bd312faa1219b7298174a2d503a2d98
Max Beneke <max.beneke@gmail.com>
2021-06-25 16:15:10 -0400

    made a dijkstra blog doc

--- colt-steele/Data Structures/binaryHeaps.js
+++ colt-steele/Data Structures/binaryHeaps.js
@@ -2,16 +2,18 @@ class MaxBinaryHeap {
     constructor() {
         this.values = [];
     }
-    insert(element) {
+    enqueue(element) {
         this.values.push(element);
         this.bubbleUp();
     }
     bubbleUp() {
         let idx = this.values.length - 1;
         const element = this.values[idx];
+        
         while(idx > 0) {
             let parentIdx = Math.floor((idx - 1)/2)
             let parent = this.values[parentIdx];
+
             if (element <= parent) break;
             if (element > parent) {
                 this.values[parentIdx] = element;
@@ -20,7 +22,7 @@ class MaxBinaryHeap {
             }
         }
     }
-    extractMax() {
+    dequeue() {
         let max = this.values[0];
         let end = this.values.pop();
         if (this.values.length > 0) {
