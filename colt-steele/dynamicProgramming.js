3d8d6b9648278a7fa212618a068822a530d96a4b
Max Beneke <max.beneke@gmail.com>
2021-06-30 11:42:25 -0400

    fib w/ memoization

--- colt-steele/dynamicProgramming.js
+++ colt-steele/dynamicProgramming.js
@@ -3,4 +3,14 @@ function fib(n) {
     return fib(n-1) + fib(n-2);
 }
 
-console.log(fib(20))
+
+
+function DPFib(n, memo=[]) {
+    if (memo[n] !== undefined) return memo[n];
+    if (n <= 2) return 1;
+    var res = fib(n-1, memo) + fib(n-2, memo);
+    memo[n] = res;
+    return res;
+}
+
+console.log(DPFib(20))
